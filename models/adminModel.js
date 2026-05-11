22665a44d221fef7e59e3c16938c567a2731f1b6
Muzzayan Omaar <muzzayanomaar@gmail.com>
2026-05-12 02:22:19 +0400

    fixing login logic

--- models/adminModel.js
+++ models/adminModel.js
@@ -1,13 +1,20 @@
 import mongoose from "mongoose";
 
-const adminSchema = new mongoose.Schema(
-  {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, default: "admin" },
+const adminSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  { timestamps: true }
-);
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    default: "admin",
+  },
+});
 
 const Admin = mongoose.model("Admin", adminSchema);
 
