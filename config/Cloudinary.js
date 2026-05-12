b42c68d91580f5d386a529c451f6913fb7f09553
Muzzayan Omaar <muzzayanomaar@gmail.com>
2026-05-12 14:41:39 +0400

    adding pdt availability and direct img upload12

--- config/Cloudinary.js
+++ config/Cloudinary.js
@@ -1,9 +1,24 @@
 import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 
+// ======================
+// CLOUDINARY CONFIG
+// ======================
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+// ======================
+// MULTER STORAGE
+// ======================
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "eyenet-products",
+    allowed_formats: ["jpg", "jpeg", "png", "webp"],
+  },
+});
+
+export { cloudinary, storage };
